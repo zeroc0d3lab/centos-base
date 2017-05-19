45d96d927eafb0cc12d62371b432992c82af22bb
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-05-19 13:57:06 +0700

    Update test for docker_test and validation check running

--- tests/docker_tests.sh
+++ tests/docker_tests.sh
@@ -15,4 +15,5 @@ env | sort
 echo "FROM ${DOCKER_IMAGE}:${DOCKER_TAG}" > Dockerfile.test &&
 docker build -t "${DOCKER_IMAGE}:${DOCKER_TAG}-test" -f Dockerfile.test . &&
 docker run -it --rm -v $(pwd)/tests/results:/var/tests/results "${DOCKER_IMAGE}:${DOCKER_TAG}-test" &&
+docker ps -a
 rm Dockerfile.test
