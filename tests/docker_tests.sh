9f6a9dfa0c1628e1e307d4cf9a46256bd7421762
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-05-19 10:19:06 +0700

    Add docker_tests.sh for testing container by TravisCI

---
+++ tests/docker_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE="zeroc0d3/centos-base"
+DOCKER_TAG="latest"
+
+#### Halt script on error
+set -e
+
+echo '##### Print docker version'
+docker --version
+
+echo '##### Print environment'
+env | sort
+
+echo "FROM ${DOCKER_IMAGE}:${DOCKER_TAG}" > Dockerfile.test &&
+docker build -t "${DOCKER_IMAGE}:${DOCKER_TAG}-test" -f Dockerfile.test . &&
+docker run -it --rm -v $(pwd)/tests/results:/var/tests/results "${DOCKER_IMAGE}:${DOCKER_TAG}-test" &&
+rm Dockerfile.test
